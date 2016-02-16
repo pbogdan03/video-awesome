89130fdfb015d93c683991cd465334a7aac20cd5
Bogdan Pestritu <bogdan.pestritu@rga.com>
2016-02-16 15:43:46 +0200

    added canvid.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,65 @@
 import $ from 'jquery';
+import canvid from 'canvid';
 import stylesheet from './styles.scss';
 import videoJs from './components/video/video';
 import videoHbs from './components/video/video.hbs';
 
 videoJs();
 
-document.addEventListener('DOMContentLoaded', function(){
-    var v = document.querySelectorAll('.video-container__player')[0];
-    var canvas = document.querySelectorAll('.canvas-video')[0];
-    var beginBtn = document.querySelectorAll('.video-container__begin')[0];
-    var context = canvas.getContext('2d');
+$('body').prepend(videoHbs);
 
-    beginBtn.addEventListener('click', function(e) {
-        v.play();
-        beginBtn.blur();
-    }, false);
-
-    var cw = canvas.clientWidth;
-    var ch = canvas.clientHeight;
-    canvas.width = cw;
-    canvas.height = ch;
-
-    v.addEventListener('play', function(){
-        draw(this,context,cw,ch);
-    },false);
-
-},false);
-
-function draw(v,c,w,h) {
-    if(v.paused || v.ended) return false;
-    c.drawImage(v,0,0,w,h);
-    setTimeout(draw,20,v,c,w,h);
+var imageSprites = {};
+for(var i = 0; i <= 9; i++) {
+    if(i < 9) {
+        imageSprites['clip0' + i] = {
+            src: './assets/frames/bigbuckbunny0' + i + '.jpg',
+            frames: 36,
+            cols: 6,
+            loops: 1,
+            nextClip: 'clip0' + (i + 1),
+            onEnd: function() {
+                canvidControl.play(this.nextClip);
+            }
+        };
+    } else if(i === 9) {
+        imageSprites['clip0' + i] = {
+            src: './assets/frames/bigbuckbunny0' + i + '.jpg',
+            frames: 36,
+            cols: 6,
+            loops: 1,
+            nextClip: 'clip' + (i + 1),
+            // onEnd: function() {
+            //     canvidControl.play(this.nextClip);
+            // }
+        };
+    } else if(i === 82) {
+        imageSprites['clip' + i] = {
+            src: './assets/frames/bigbuckbunny' + i + '.jpg',
+            frames: 36,
+            cols: 6,
+            loops: 1
+        };
+    } else {
+        imageSprites['clip' + i] = {
+            src: './assets/frames/bigbuckbunny' + i + '.jpg',
+            frames: 36,
+            cols: 6,
+            loops: 1,
+            nextClip: 'clip' + (i + 1),
+            onEnd: function() {
+                canvidControl.play(this.nextClip);
+            }
+        };
+    }
 }
 
-$('body').prepend(videoHbs);
+var canvidControl = canvid({
+    selector : '.canvas-video',
+    videos: imageSprites,
+    loaded: function() {
+        $('.spinner').hide();
+        canvidControl.play('clip00');
+    }
+});
 
 console.log('index.js loaded..');
