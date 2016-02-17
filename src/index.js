59c34457fe35f45ed96e72be8ece5111bff0309b
Bogdan Pestritu <bogdan.pestritu@rga.com>
2016-02-17 14:45:31 +0200

    added image sequence how to

--- src/index.js
+++ src/index.js
@@ -8,58 +8,64 @@ videoJs();
 
 $('body').prepend(videoHbs);
 
-var imageSprites = {};
-for(var i = 0; i <= 9; i++) {
-    if(i < 9) {
-        imageSprites['clip0' + i] = {
-            src: './assets/frames/bigbuckbunny0' + i + '.jpg',
-            frames: 36,
-            cols: 6,
-            loops: 1,
-            nextClip: 'clip0' + (i + 1),
-            onEnd: function() {
-                canvidControl.play(this.nextClip);
-            }
-        };
-    } else if(i === 9) {
-        imageSprites['clip0' + i] = {
-            src: './assets/frames/bigbuckbunny0' + i + '.jpg',
-            frames: 36,
-            cols: 6,
-            loops: 1,
-            nextClip: 'clip' + (i + 1),
-            // onEnd: function() {
-            //     canvidControl.play(this.nextClip);
-            // }
-        };
-    } else if(i === 82) {
-        imageSprites['clip' + i] = {
-            src: './assets/frames/bigbuckbunny' + i + '.jpg',
-            frames: 36,
-            cols: 6,
-            loops: 1
-        };
-    } else {
-        imageSprites['clip' + i] = {
-            src: './assets/frames/bigbuckbunny' + i + '.jpg',
-            frames: 36,
-            cols: 6,
-            loops: 1,
-            nextClip: 'clip' + (i + 1),
-            onEnd: function() {
-                canvidControl.play(this.nextClip);
-            }
-        };
-    }
-}
+// var imageSprites = {};
+// for(var i = 0; i <= 47; i++) {
+//     if(i < 9) {
+//         imageSprites['clip0' + i] = {
+//             src: './assets/frames/optimized/aliendesert0' + i + '.jpg',
+//             frames: 100,
+//             cols: 10,
+//             fps: 30,
+//             loops: 1,
+//             nextClip: 'clip0' + (i + 1),
+//             onEnd: function() {
+//                 canvidControl.play(this.nextClip);
+//             }
+//         };
+//     } else if(i === 9) {
+//         imageSprites['clip0' + i] = {
+//             src: './assets/frames/optimized/aliendesert0' + i + '.jpg',
+//             frames: 100,
+//             cols: 10,
+//             fps: 30,
+//             loops: 1,
+//             nextClip: 'clip' + (i + 1),
+//             onEnd: function() {
+//                 canvidControl.play(this.nextClip);
+//             }
+//         };
+//     } else if(i === 47) {
+//         imageSprites['clip' + i] = {
+//             src: './assets/frames/optimized/aliendesert' + i + '.jpg',
+//             frames: 100,
+//             cols: 10,
+//             fps: 30,
+//             loops: 1
+//         };
+//     } else {
+//         imageSprites['clip' + i] = {
+//             src: './assets/frames/optimized/aliendesert' + i + '.jpg',
+//             frames: 100,
+//             cols: 10,
+//             fps: 30,
+//             loops: 1,
+//             nextClip: 'clip' + (i + 1),
+//             onEnd: function() {
+//                 canvidControl.play(this.nextClip);
+//             }
+//         };
+//     }
+// }
 
-var canvidControl = canvid({
-    selector : '.canvas-video',
-    videos: imageSprites,
-    loaded: function() {
-        $('.spinner').hide();
-        canvidControl.play('clip00');
-    }
-});
+// console.log(imageSprites.clip00);
+
+// var canvidControl = canvid({
+//     selector : '.canvas-video',
+//     videos: imageSprites,
+//     loaded: function() {
+//         $('.spinner').hide();
+//         canvidControl.play('clip00');
+//     }
+// });
 
 console.log('index.js loaded..');
